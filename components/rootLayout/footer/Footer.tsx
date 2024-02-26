d855ce67b1e43d6a50966a4b3c43df27cf7b74d3
eeennsu <116697733+eeennsu@users.noreply.github.com>
2024-02-26 17:51:05 +0900

    Footer 반응형 디자인 수정

--- components/rootLayout/footer/Footer.tsx
+++ components/rootLayout/footer/Footer.tsx
@@ -7,8 +7,8 @@ import SNSMenuItem from '../header/SNSMenuItem';
 const Footer: FC = () => {
 
     return (
-        <footer className='flex justify-center py-4 bg-slate-200 sm:py-4'>
-            <div className='flex flex-col w-full max-w-5xl px-4 sm:justify-between max-sm:px-6 sm:flex-row'>       
+        <footer className='flex justify-center py-4 bg-white sm:py-4'>
+            <div className='flex flex-col w-full px-4 md:max-w-4xl xl:max-w-5xl sm:justify-center lg:gap-24 xl:gap-40 max-sm:px-6 sm:flex-row'>       
                 <div className='items-center hidden sm:flex'>
                     <div className='flex flex-col gap-2 text-sm'>
                         {
--- components/rootLayout/footer/FooterInfoItem.tsx
+++ components/rootLayout/footer/FooterInfoItem.tsx
@@ -8,14 +8,14 @@ type Props = {
 const FooterInfoItem: FC<Props> = ({ info }) => {
 
     return (
-        <div className='flex items-center sm:grid sm:grid-cols-5 max-sm:gap-2'>                                
+        <div className='flex items-center sm:grid sm:grid-cols-4 md:grid-cols-5 max-lg:gap-4'>                                
             <p className='flex gap-2.5 sm:col-span-1'>    
                 {info.icon}                                    
                 <span className='max-sm:hidden'>
                     {info.name} 
                 </span>
             </p>
-            <p className='text-xs font-semibold sm:col-span-4'>
+            <p className='text-xs font-semibold sm:col-span-3 md:col-span-4 max-lg:max-w-[270px]'>
                 {info.value}
             </p>
         </div>
